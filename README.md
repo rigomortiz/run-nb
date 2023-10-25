267e7133d9722e12716264906d517300c0b52389
Rigoberto Canseco López <rigoberto.canseco.lopez.contractor@bbva.com>
2023-10-25 12:45:30 -0600

    Version 1.0

---
+++ README.md
@@ -0,0 +1,4 @@
+# run-nb
+Proyecto en python para ejecutar notebooks de jupyter de forma automática.
+
+## Instalación
--- src/TestMagic.py
+++ src/TestMagic.py
@@ -27,6 +27,23 @@ class TestMagic(Magics):
         os.remove(test_file)
         TEST_COUNT = TEST_COUNT + 1
 
+    @register_line_cell_magic
+    def save_test(self, cell):
+        print('Saving tests ...')
+        test_file = OUTPUT_PATH + 'test.py'
+        with open(test_file, 'a') as f:
+            f.write(cell.format(**globals()))
+            f.close()
+        print(f"Finish saving tests in {test_file}")
+
+    @register_line_magic
+    def run_all_test(self):
+        print('Running all tests ...')
+        test_file = OUTPUT_PATH + 'test.py'
+        pytest.main(['--no-header', '-vv', '-s', '-rA', '--color=yes', '--code-highlight=yes', test_file])
+        print("Finish tests")
+        os.remove(test_file)
+
 
 def load_ipython_extension(ipython):
     ipython.register_magics(TestMagic)
