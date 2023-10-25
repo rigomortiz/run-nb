be6218a5eba162214bdae0f652c8cf3b9a71f055
Rigoberto Canseco López <rigoberto.canseco.lopez.contractor@bbva.com>
2023-10-25 10:19:03 -0600

    Version 1.0

--- src/Utils.py
+++ src/Utils.py
@@ -1,11 +1,9 @@
 import json
 import logging
-
+import os
 import numpy as np
 import nbformat
-from src.Executor.Constants import *
-
-
+from src import Constants
 class Utils:
     @staticmethod
     def read_config_file(file_path: str) -> dict or None:
@@ -14,7 +12,6 @@ class Utils:
             return json.load(file)
         except FileNotFoundError:
             return None
-
     @staticmethod
     def read_env_file(file_path: str) -> dict or None:
         try:
@@ -27,10 +24,9 @@ class Utils:
             return args
         except FileNotFoundError:
             return None
-
     @staticmethod
     def write_params_to_notebook(params):
-        with open(INPUT_PATH + os.sep + ENV_FILE, MODE) as file:
+        with open(Constants.INPUT_PATH + os.sep + Constants.ENV_FILE, Constants.MODE) as file:
             for param in params:
                 file.write(param + '\n')
 
@@ -41,7 +37,7 @@ class Utils:
     @staticmethod
     def set_env(params: list) -> None:
         for param in params:
-            os.environ[param[NAME]] = param[VALUE]
+            os.environ[param[Constants.NAME]] = param[Constants.VALUE]
 
     @staticmethod
     def clean_env() -> None:
@@ -49,16 +45,15 @@ class Utils:
 
     @staticmethod
     def create_directories(folder) -> None:
-        if not os.path.exists(OUTPUT_PATH):
-            os.makedirs(OUTPUT_PATH)
-        if not os.path.exists(OUTPUT_PATH + os.sep + folder):
-            os.makedirs(OUTPUT_PATH + os.sep + folder)
+        if not os.path.exists(Constants.OUTPUT):
+            os.makedirs(Constants.OUTPUT)
+        if not os.path.exists(Constants.OUTPUT + os.sep + folder):
+            os.makedirs(Constants.OUTPUT + os.sep + folder)
 
     @staticmethod
     def get_cell_read_env() -> nbformat.notebooknode.NotebookNode:
         source = '''
 import os
-
 file = open('.env')
 args = dict()
 for line in file:
@@ -72,64 +67,22 @@ for line in file:
     def end_cell() -> nbformat.notebooknode.NotebookNode:
         source = '''
 import os
-
 with open(PATH + os.path.sep + 'variables.env', 'w') as file:
     print('Saving .env ...')
     vs = [[x.replace('@', ''), os.environ[x]] for x in list(os.environ.copy().keys()) if x[0] == '@' and x[-1] == '@']
     for v in vs:
         file.write(v[0] + '=' + v[1] + "\\n")
-
     '''
-
         return nbformat.v4.new_code_cell(source)
 
     @staticmethod
     def start_cell(params) -> nbformat.notebooknode.NotebookNode:
-        ps = ''.join(['%env ' + param[NAME] + ' = '  + param[VALUE] + '\n' for param in params])
-        source = '''
-from IPython.core.magic import register_line_cell_magic
-from IPython import get_ipython
-import pytest
-import os
-global TEST_COUNT
-TEST_COUNT = 1
-'''
+        ps = "# Variables Globales\n"
+        ps += ''.join([key + ' = \'' + params[key] + '\'\n' for key in params])
+        with open('src/TestMagic.py') as file:
+            source = file.read()
 
-        source += ps + '''
-        
-PATH = %env @PATH@        
-if os.path.exists(PATH + os.path.sep + 'test.py'):
-    os.remove(PATH + os.path.sep + 'test.py')
-@register_line_cell_magic
-def test(line, cell):
-    print('Running tests ...')
-    #print(globals()['In'])
-    #print(get_ipython().user_ns)
-    global TEST_COUNT
-    test_file = PATH + os.path.sep + 'test-tmp' + str(TEST_COUNT) + '.py'
-    if cell is None:
-        #print('cell')
-        with open(PATH + os.path.sep + 'test.py', 'a') as f:
-            f.write(cell.format(**globals()))
-            f.close()
-    else:
-        #print('line')
-        with open(PATH + os.path.sep + 'test.py', 'a') as f:
-            f.write(cell.format(**globals()))
-            f.close()
-    with open(test_file, 'w') as f:
-        f.write(cell.format(**globals()))
-        f.close()
-    #pytest.main(['-v', PATH + os.path.sep + 'test.py'])
-    csv_file = PATH + os.path.sep + 'In[' + str(len(globals()['In']) - 1) + '].csv'
-    csv_cols = "--csv-columns 'name,status,message,duration_formatted,function,parameters_as_columns'"
-    res = os.system('pytest --csv ' + csv_file + ' ' + PATH + os.path.sep + 'test-tmp.py')
-
-@register_line_cell_magic
-def write_test(line, cell):
-'''
-
-        return nbformat.v4.new_code_cell(source)
+        return nbformat.v4.new_code_cell(ps + source)
 
     @staticmethod
     def merge_notebooks(paths, path_output):
@@ -137,12 +90,9 @@ def write_test(line, cell):
         for p in paths:
             with open(p, 'r') as file:
                 cells.append(json.loads(file.read())['cells'])
-
         with open(paths[0], 'r') as file:
             new_dict = json.loads(file.read()).copy()
             new_dict['cells'] = list(np.concatenate(cells))
-
         with open(path_output, 'w') as json_file:
             json.dump(new_dict, json_file)
-
         return True
